4d6dcc5cf0c7de2a025c3323a2da5ddce942d516
Shraddha Porwal <shraddhaporwal@gmail.com>
2016-05-09 15:23:34 +0530

    Update user email added in settings screen

--- app/controllers/Settings.js
+++ app/controllers/Settings.js
@@ -1,8 +1,76 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
+var nsSettings = {};
 
-var thisUser = Kinvey.getActiveUser();
+nsSettings.showHideHint = function(label, txtField) {
+	label.visible = (txtField.value.trim() == "");
+};
 
-console.log(thisUser);
+$.emailField.addEventListener('change', function() {
+	nsSettings.showHideHint($.lblHint_email, $.emailField);
+});
 
-$.email.text = thisUser.username;
+$.edit_email_btn.addEventListener('click', function() {
+	var thisUser = Kinvey.getActiveUser();
+	$.emailField.value = thisUser.username;
+
+	$.edit_email_view.height = Titanium.UI.SIZE;
+	nsSettings.showHideHint($.lblHint_email, $.emailField);
+	$.email.height = 0;
+	$.email.visible = false;
+	$.edit_email_btn.height = 0;
+	$.save_email_btn.height = 15;
+});
+
+$.save_email_btn.addEventListener('click', function() {
+
+	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+	var emailField = $.emailField.getValue();
+	console.debug(emailField);
+	console.debug("Email validate ", reg.test(emailField).toString());
+
+	if (reg.test(emailField)) {
+
+		var serviceCalls = require("serverCalls");
+
+		//Login
+		var emailField = $.emailField.getValue();
+
+		this.onloadCallback = function(user) {
+
+			var thisUser = Kinvey.getActiveUser();
+			$.email.text = thisUser.username;
+
+			$.edit_email_view.height = 0;
+			$.email.height = Titanium.UI.SIZE;
+			$.email.visible = true;
+			$.save_email_btn.height = 0;
+			$.edit_email_btn.height = 15;
+			Alloy.Globals.loading.hide();
+		};
+
+		this.onerrorCallback = function(error) {
+			console.debug("Error occured in updating email");
+			alert(L('err_serviceError'));
+			Alloy.Globals.loading.hide();
+		};
+
+		Alloy.Globals.loading.show();
+		var updateEmail = new serviceCalls.updateUser(emailField, this.onloadCallback, this.onerrorCallback);
+
+	} else {
+		alert(L('err_loginDetails'));
+	}
+
+});
+
+nsSettings.init = function() {
+
+	var thisUser = Kinvey.getActiveUser();
+	console.log(thisUser);
+	$.email.text = thisUser.username;
+	$.save_email_btn.height = 0;
+};
+
+nsSettings.init();
