8d0c315b0b0b879116ce7a4fd9902e7521f2dffa
Shraddha Porwal <shraddhaporwal@gmail.com>
2017-11-28 12:16:55 +0530

    Updated landing page to PRB website

--- app/controllers/main/home_wrapper.js
+++ app/controllers/main/home_wrapper.js
@@ -3,4 +3,10 @@ var args = arguments[0] || {};
 Alloy.Globals.pageflow = $.pageFlow;
 Alloy.Globals.home = true;
 
-Alloy.Globals.openWindow('MainView', null, false, null, 'misc/center_logo');
+// Alloy.Globals.openWindow('MainView', null, false, null, 'misc/center_logo');
+Alloy.Globals.openWindow('GenericWebView', {
+		url : "https://punkrockbowling.com/",
+		addBanner : true,
+		// screen : 'bowling'
+	}, false, null, 'misc/center_logo');
+
